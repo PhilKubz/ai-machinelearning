7b064abaef7b3ebb35368346afd2b46b39058763
PhilKubsz <philip.kubisz@gmail.com>
2023-11-13 21:33:27 -0800

    Project: Add instructions to build on inputs / random selection slot-machine

--- Python/Project/main.py
+++ Python/Project/main.py
@@ -1,3 +1,44 @@
+import random
+
+
+# Currency Values
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+# Slot Machine Values
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    # Can use "_" instead of i or another variable if we do not need it for incrementing or decrementing 
+    for _ in range(cols):
+        column = []
+        # "[:] allows to create a copy of a list instead of referncing the object directly"
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            #append in Python is like Push in an array in JS
+            columns.append(value)
+
+        columns.append(column)
+
+    return columns
+
 def deposit():
     while True:
         amount = input("What would you like to depost? $")
@@ -11,7 +52,47 @@ def deposit():
             print("Please enter a number.")
     return amount
 
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + "). ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines")
+        else:
+            print("Please enter a number.")
+    return lines
+
+
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}. ")
+        else:
+            print("Please enter a number.")
+    return amount
+
+
 def main():
     balance = deposit()
-    print(balance)
+    lines = get_number_of_lines()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You do not have enough to bet that amount. You current balance is: ${balance}")
+        else:
+            break
+
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}. ")
+
+
 main()
